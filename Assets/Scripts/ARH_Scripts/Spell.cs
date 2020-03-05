51f158fe1e6605a6c537174ca3ba26a917a5275f
Gyotek <a.herbreteau@rubika-edu.com>
2020-03-05 21:57:17 +0100

    c pa bo

--- Assets/Scripts/ARH_Scripts/Spell.cs
+++ Assets/Scripts/ARH_Scripts/Spell.cs
@@ -6,19 +6,49 @@ public class Spell : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb = default;
     [SerializeField] Transform tr = default;
+    [SerializeField] SpriteRenderer sprite = default;
     [SerializeField] private float spellSpeed = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        //rb.velocity = Vector3.zero;
+        //transform.LookAt(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y));
+
+        switch (SpellManager.instance.actualElement)
+        {
+            case SpellManager.SpellElement.Fire:
+                sprite.color = Color.red;
+                break;
+            case SpellManager.SpellElement.Water:
+                sprite.color = Color.blue;
+                break;
+            case SpellManager.SpellElement.Plant:
+                sprite.color = Color.green;
+                break;
+            default:
+                break;
+        }
+
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 dir = (mousePos - pos);
+        rb.AddForce(dir * spellSpeed * 100);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = new Vector2 (spellSpeed, 0);
-        Debug.Log(rb.velocity);
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (rb.sharedMaterial != null)
+        {
+            rb.sharedMaterial = null;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
--- Assets/Scripts/ARH_Scripts/SpellManager.cs
+++ Assets/Scripts/ARH_Scripts/SpellManager.cs
@@ -2,22 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Sirenix.OdinInspector;
 
-public class SpellManager : MonoBehaviour
+public class SpellManager : SerializedMonoBehaviour
 {
     public static SpellManager instance;
     private void Awake() { instance = this; }
 
-    public enum SpellElement { Fire, Water, Plant };
-    public enum SpelZone { Line, Circle, Multiple };
-    public enum SpellType { Impact, Bounce, Perforant };
+    public enum SpellElement { Fire, Water, Plant, ERROR };
+    public enum SpellZone { Line, Circle, Multiple, ERROR };
+    public enum SpellType { Impact, Bounce, Perforant, ERROR };
 
-    SpellElement actualElement = default;
-    SpelZone actualZone = default;
-    SpellType actualType = default;
+    public SpellElement actualElement = default;
+    public SpellZone actualZone = default;
+    public SpellType actualType = default;
 
-    [SerializeField] Transform spellLauncherPos;
+    [SerializeField] Transform spellLauncher;
     [SerializeField] bool isAiming = true;
+    [SerializeField] GameObject spellPrefab = default;
+
+    public Dictionary<Image, SpellElement> ElementsCurseurs = new Dictionary<Image, SpellElement>();
+    public Dictionary<Image, SpellZone> ZoneCurseurs = new Dictionary<Image, SpellZone>();
+    public Dictionary<Image, SpellType> TypeCurseurs = new Dictionary<Image, SpellType>();
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +34,7 @@ public class SpellManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpellAiming(spellLauncherPos.position);
+        SpellAiming(spellLauncher.position);
     }
 
     void SpellAiming(Vector2 pos)
@@ -38,12 +44,58 @@ public class SpellManager : MonoBehaviour
         Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 dir = (mousePos - pos);
         Debug.DrawRay(pos, pos + dir * 100, Color.blue);
-
+        Debug.Log(ElementChecker() + " + " + ZoneChecker() + " + " + TypeChecker());
+        if (Input.GetMouseButtonDown(0) && (ElementChecker() != SpellElement.ERROR && ZoneChecker() != SpellZone.ERROR && TypeChecker() != SpellType.ERROR))
+        {
+            Debug.Log("Spell launched");
+            LaunchSpell(pos);
+            isAiming = false;
+        }
     }
 
-    void LaunchSpell()
+    void LaunchSpell(Vector2 pos)
     {
-        Debug.Log(actualElement + " + " + actualZone + " + " + actualType);
+        actualElement = ElementChecker();
+        actualZone = ZoneChecker();
+        actualType = TypeChecker();
+
+        Debug.Log(ElementChecker() + " + " + ZoneChecker() + " + " + TypeChecker());
+        Instantiate(spellPrefab, new Vector3(pos.x, pos.y, 0), Quaternion.identity);
+        isAiming = true;
+    }
+
+    SpellElement ElementChecker()
+    {
+        foreach (KeyValuePair<Image, SpellElement> elementCurseur in ElementsCurseurs)
+        {
+            if (elementCurseur.Key.enabled)
+            {
+                return elementCurseur.Value;
+            }
+        }
+        return SpellElement.ERROR;
+    }
+    SpellZone ZoneChecker()
+    {
+        foreach (KeyValuePair<Image, SpellZone> zoneCurseur in ZoneCurseurs)
+        {
+            if (zoneCurseur.Key.enabled)
+            {
+                return zoneCurseur.Value;
+            }
+        }
+        return SpellZone.ERROR;
+    }
+    SpellType TypeChecker()
+    {
+        foreach (KeyValuePair<Image, SpellType >  typeCurseur in TypeCurseurs)
+        {
+            if (typeCurseur.Key.enabled)
+            {
+                return typeCurseur.Value;
+            }
+        }
+        return SpellType.ERROR;
     }
 
     public void SetSpellParameter(Text text)
@@ -61,13 +113,13 @@ public class SpellManager : MonoBehaviour
                 break;
 
             case ("Circle"):
-                SetSpellParameter(SpelZone.Circle);
+                SetSpellParameter(SpellZone.Circle);
                 break;
             case ("Multi"):
-                SetSpellParameter(SpelZone.Multiple);
+                SetSpellParameter(SpellZone.Multiple);
                 break;
             case ("Line"):
-                SetSpellParameter(SpelZone.Line);
+                SetSpellParameter(SpellZone.Line);
                 break;
 
             case ("Impact"):
@@ -87,7 +139,7 @@ public class SpellManager : MonoBehaviour
     {
         actualElement = parameter;
     }
-    public void SetSpellParameter(SpelZone parameter)
+    public void SetSpellParameter(SpellZone parameter)
     {
         actualZone = parameter;
     }
