e9e13b9df49d3156224e692d4ab6c6be38d7fb20
AdrienGD <adrien.dourgnon@gmail.com>
2020-03-06 15:41:24 +0100

    DataCalculator, SliderPhase

--- Assets/Scripts/AD_Script/DataCalculator.cs
+++ Assets/Scripts/AD_Script/DataCalculator.cs
@@ -5,21 +5,61 @@ using UnityEngine.UI;
 
 
 public class DataCalculator : MonoBehaviour
-    {
+{
 
-    public Slider creativity;
-    public Slider transposition;
-    public Slider adaptability;
-    public Slider flexibility;
+  
+    public ScriptableObject data;
+
+    //Creativity 
+    public float Cp1;
+    public float Cp2;
+    public float Cp3;
+    public float Cf;
+
+    //Transposition 
 
 
+    //Adaptability
+    public float averageTurnDuration;
+    public float timeRatio;
+    public float timeIndicator;
+
+    //Flexibility
+
+    public float finalFlex;
 
 
     public void CreativityRate()
     {
+        // Phase 1
+        Cp1 = (GetComponent<Data>().P1numberOfDifferentSpell / GetComponent<Data>().P1numberTotalOfSpell) * 100;
 
+        // Phase 2
+        Cp2 = (GetComponent<Data>().P2numberOfSpell / GetComponent<Data>().P2numberTotalOfSpell) * 100;
 
+        // Phase 3
+        Cp3 = (GetComponent<Data>().P3numberOfSpell / GetComponent<Data>().P3numberTotalOfSpell) * 100;
+
+        //Average
+        Cf = (Cp1 + Cp2 + Cp3) / 3;
 
     }
-     
+
+    public void TranspositionRate()
+    {
+        averageTurnDuration = (GetComponent<Data>().playerTurnTime / GetComponent<Data>().playterTurn);
+
+        timeRatio = (averageTurnDuration / GetComponent<Data>().maxPlayerTurnTime) * 100;
+
+        timeIndicator = 100 - timeRatio;
     }
+
+
+    public void FlexibilityRate()
+    {
+        finalFlex = (timeIndicator + Cf) / 3;
+
+    }
+
+
+}
---
+++ Assets/Scripts/AD_Script/SliderPhase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
+
+public class SliderPhase : MonoBehaviour
+{
+    public DataCalculator calculator;
+
+    public Slider creativity;
+    public Slider transposition;
+    public Slider adaptability;
+    public Slider flexibility;
+
+    public float fillSpeed = 1f; 
+
+
+    public void SliderTime()
+    {
+
+        SceneManager.LoadScene("AD_StatScene");
+
+        StartCoroutine(SliderDisplay());
+
+    }
+
+    public IEnumerator SliderDisplay()
+    {
+        new WaitForSeconds(2f);
+
+        creativity.value = Mathf.Lerp(0, GetComponent<DataCalculator>().Cf, fillSpeed);
+
+        new WaitForSeconds(2f);
+
+        transposition.value = Mathf.Lerp(0, GetComponent<DataCalculator>().timeIndicator, fillSpeed);
+
+        new WaitForSeconds(2f);
+
+        flexibility.value = Mathf.Lerp(0, GetComponent<DataCalculator>().finalFlex, fillSpeed);
+
+        yield break; 
+    }
+
+}
