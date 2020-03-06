83fd1760c40a6c153ef2214dc303f15527c8f476
AdrienGD <adrien.dourgnon@gmail.com>
2020-03-06 14:28:56 +0100

    DataCalculator Script

---
+++ Assets/Scripts/AD_Script/DataCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DataCalculator : MonoBehaviour
+    {
+
+    public Slider creativity;
+    public Slider transposition;
+    public Slider adaptability;
+    public Slider flexibility;
+
+
+
+
+    public void CreativityRate()
+    {
+
+
+
+    }
+     
+    }
